df26d68d2413b020ffdb90378696f8fab4aa9e92
Vishal Sharma <103028557+Vishal3680@users.noreply.github.com>
2022-12-15 21:24:06 +0530

    Add files via upload

---
+++ Aggressive_Cows.cpp
@@ -0,0 +1,37 @@
+bool isPossible(vector<int> &stalls, int k , int mid){
+    int CowCount = 1;
+    int lastPos = stalls[0];
+    for(int i = 0 ; i< stalls.size(); i++){
+        if(stalls[i]-lastPos >= mid){
+            CowCount++;
+            if(CowCount == k){
+                return true;
+             }
+            lastPos = stalls[i];
+        }
+    }
+    return false;
+}
+
+int aggressiveCows(vector<int> &stalls, int k)    
+{
+    //    Write your code here.
+    int ans = -1;
+    sort(stalls.begin(),stalls.end());
+    int s = 0;
+    int maxi = -1;
+    for(int i = 0 ; i<stalls.size() ; i++){
+        maxi = max(maxi,stalls[i]);
+    }
+    int e = maxi;
+    while(s<=e){
+        int mid = s + ( e - s)/2;
+        if(isPossible(stalls,k,mid)){
+            ans = mid ;
+            s = mid + 1;
+        }else{
+            e = mid -1;
+        }
+    }
+    return ans;
+}
